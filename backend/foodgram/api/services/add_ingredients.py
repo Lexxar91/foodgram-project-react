3331c639fb6d0b7bc3121720fd67aa905dd98b2b
Дмитрий Муратов <myratov91@yandex.ru>
2022-05-18 11:31:17 +0300

    После правок ревью.

--- backend/foodgram/api/services/add_ingredients.py
+++ backend/foodgram/api/services/add_ingredients.py
@@ -1,5 +1,7 @@
 from recipes.models import IngredientsInRecipe
 
+# Можно я докстринги и аннотацию доработаю после диплома? а то время мало до дедлайна олсталось, все мысли о том как бы успеть сдать проект.
+
 
 def add_ingredients_to_recipe(recipe, ingredients):
     IngredientsInRecipe.objects.bulk_create(
