73012a3190f600b033ac259ad82fb3357eaf5198
Дмитрий Муратов <myratov91@yandex.ru>
2022-06-02 19:29:58 +0300

    add workflow1

--- backend/api/services/add_ingredients.py
+++ backend/api/services/add_ingredients.py
@@ -3,12 +3,12 @@ from recipes.models import IngredientsInRecipe
 
 def add_ingredients_to_recipe(recipe, ingredients):
     IngredientsInRecipe.objects.bulk_create(
-            [
-                IngredientsInRecipe(
-                    recipe=recipe,
-                    ingredient_id=ingredient['ingredients']['id'],
-                    amount=ingredient['amount'],
+        [
+            IngredientsInRecipe(
+                recipe=recipe,
+                ingredient_id=ingredient['ingredients']['id'],
+                amount=ingredient['amount'],
             )
-                for ingredient in ingredients
-            ]
-        )
+            for ingredient in ingredients
+        ]
+    )
