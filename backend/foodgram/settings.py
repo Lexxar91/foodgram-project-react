ad01fe9bbec81a6ad9cfbb55298937dd6be3d949
Дмитрий Муратов <myratov91@yandex.ru>
2022-06-03 21:50:14 +0300

    add workflow2.2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-vs9a)!)_$k5-g09s&k%^@ibb2u8b6&0(i*_obifq29zu9z66-+')
+SECRET_KEY = 'django-insecure-vs9a)!)_$k5-g09s&k%^@ibb2u8b6&0(i*_obifq29zu9z66-+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG'))
